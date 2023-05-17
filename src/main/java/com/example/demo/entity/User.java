6debd48b2fadc0a30b73bab6e8799bed3fceeff6
Leonid Karlovskii <karlovskiyleo@gmail.com>
2023-05-17 10:55:40 +0300

    Start building security

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -5,6 +5,7 @@ import com.example.demo.entity.enums.ERole;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -13,7 +14,7 @@ import java.util.*;
 
 @Data
 @Entity
-public class User {
+public class User implements UserDetails {
 
 
     @Id
@@ -49,10 +50,47 @@ public class User {
     private Collection<? extends GrantedAuthority> authorities;
 
 
+    public User(Long id,
+                String username,
+                String email,
+                String password,
+                Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
     @PrePersist
     protected void onCreate() {
         this.createdDate = LocalDateTime.now();
     }
 
 
+    @Override
+    public String getPassword(){
+        return password;
+    };
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
